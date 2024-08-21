f6bcf811819efb6ebea2ddd4ffb7856c7911343d
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-08-21 18:14:28 +0530

    Initial Commit

---
+++ bluemoon123/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bluemoon123
+//
+//  Created by Test Ios on 21/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bluemoon123/bluemoon123App.swift
@@ -0,0 +1,17 @@
+//
+//  bluemoon123App.swift
+//  bluemoon123
+//
+//  Created by Test Ios on 21/08/24.
+//
+
+import SwiftUI
+
+@main
+struct bluemoon123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
